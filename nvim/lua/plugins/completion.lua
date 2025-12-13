77b161cc79b90924d4e76f33bdd4f903a9d6c73a
markiep <purcellmarkie@proton.me>
2025-12-12 18:46:32 -0600

    update

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -13,22 +13,27 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
+    -- preset='default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- preset='none' for no mappings
     -- All presets have the following mappings:
     -- C-space: Open menu or open docs if already open
     -- C-n/C-p or Up/Down: Select next/previous item
     -- C-e: Hide menu
     -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = {
+    keymap = {-- see :h blink-cmp-config-keymap
       preset = 'default',
-      ['<C-m>'] = { 'accept', 'fallback' },
+      ['<Tab>'] = { 'show', 'accept' },
       ['<A-h>'] = { 'show_signature' },
+    },
+    cmdline = {
+      keymap = {
+        -- recommended, as the default keymap will only show and select the next item
+        ['<Tab>'] = { 'show', 'accept' },
+      },
+      completion = {
+        menu = { auto_show = true },
+        ghost_text = { enabled = true };
+      },
     },
 
     appearance = { nerd_font_variant = 'mono' },
