cfcda6e0085105869aa0c48939d9c780b31f579a
markiep <purcellmarkie@proton.me>
2026-01-20 19:28:00 -0600

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -81,9 +81,9 @@ vim.opt.showmatch=true
 vim.opt.virtualedit:append('block')
 vim.opt.autoindent=true
 vim.opt.shiftround=true
-vim.opt.tabstop=2
-vim.opt.softtabstop=2
-vim.opt.shiftwidth=2
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
 vim.opt.expandtab=true
 vim.opt.smarttab=true
 -- colorize hardtabs and trailing spaces
