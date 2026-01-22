46f7be34792fad20a26298ed6887812efc6d2406
markiep <purcellmarkie@proton.me>
2026-01-22 06:42:54 -0600

    update

--- nvim/after/lsp/rust_analyzer.lua
+++ nvim/after/lsp/rust_analyzer.lua
@@ -2,7 +2,17 @@
 function Format()
   local file = vim.api.nvim_buf_get_name(0)
   local workspace_dir = vim.lsp.buf.list_workspace_folders()[1] or ""
-  vim.cmd(string.format("wa | !rustfmt --config-path %s %s", workspace_dir, file))
+  local cfg_file = workspace_dir.."/rustfmt.toml"
+  local cfg_args
+  if vim.loop.fs_stat(cfg_file) then
+    cfg_args = "--edition 2024 --config-path "..cfg_file
+  else
+    -- Default flags
+    cfg_args = [[
+      --edition 2024
+    ]]
+  end
+  vim.cmd(string.format("wa | !rustfmt %s %s", cfg_args, file))
 end
 -- Alias for Format()
 vim.cmd([[
