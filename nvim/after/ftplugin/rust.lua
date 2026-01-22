549d6fa1f3242299ef9fbb477fc2a3c4214bcd8b
markiep <purcellmarkie@proton.me>
2026-01-22 06:30:26 -0600

    update

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,8 +1,3 @@
 vim.opt.tabstop=4
 vim.opt.softtabstop=4
 vim.opt.shiftwidth=4
-
-vim.cmd([[
-com! Format :wa | !cargo fmt
-cnoreabbrev fmt Format
-]])
--- nvim/after/lsp/rust_analyzer.lua
+++ nvim/after/lsp/rust_analyzer.lua
@@ -1,3 +1,14 @@
+-- Format current file
+function Format()
+  local file = vim.api.nvim_buf_get_name(0)
+  local workspace_dir = vim.lsp.buf.list_workspace_folders()[1] or ""
+  vim.cmd(string.format("wa | !rustfmt --config-path %s %s", workspace_dir, file))
+end
+-- Alias for Format()
+vim.cmd([[
+com! Fmt :lua Format()
+cnoreabbrev fmt Fmt
+]])
 return {
   filetypes = {'rust'},
   settings = {
